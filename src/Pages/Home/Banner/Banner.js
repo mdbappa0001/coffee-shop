64407336a2272372e40fc977bb61767849f0e098
mdbappa0001 <abutanif366@gmail.com>
2022-04-17 20:05:15 +0600

    home section added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Banner.css';
+
+
+const Banner = () => {
+    return (
+        <section class="home" id="home">
+
+        <div class="content">
+            <h3>Fresh Coffee in the morning.</h3>
+            <button href="#" class="btn">Get Yours Now</button>
+        </div>
+
+    </section>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Header.css';
+import CustomLink from '../../Home/CustomLink/CustomLink';
+import logo from '../../../Photos/logo/logo.png';
+
 
 const Header = () => {
     return (
+        <div className='sticky top-0 bg-neutral-900 px-12 py-8 flex justify-center md:justify-between'>
+
+       <img className='logo' src={logo} alt="" />
+
         <div>
-            <h2>This is header</h2>
+            <CustomLink className='customlink' to='/'>HOME</CustomLink>
+            <CustomLink className='customlink' to='/reviews'>REVIEWS</CustomLink>
+            <CustomLink className='customlink' to='/dashboard'>DASHBOARD</CustomLink>
+            <CustomLink className='customlink' to='/blogs'>BLOGS</CustomLink>
+            <CustomLink className='customlink' to='/about'>ABOUT</CustomLink>
         </div>
+
+    </div>
     );
 };
 
